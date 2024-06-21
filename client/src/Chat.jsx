a4f9680b61c290e2cd95ec5d37ecc2679aca2fc6
piyumalBhathiya <fpiyumal450@gmail.com>
2024-06-21 22:21:28 +0530

    adding websockets & show who is online
    adding websockets & show who is online

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,21 +1,77 @@
-export default function Chat(){
-    return(
-        <div className="flex h-screen">
-            <div className="bg-white w-1/3">
-                contacts
-            </div>
-            <div className="flex flex-col bg-blue-50 w-2/3 p-2">
-            <div className="fldex-grow">messages</div>
-            <div className="flex gap-2 ">
-                <input type="text" placeholder="type your message here" 
-                className="bg-white flex-grow border rounded-sm p-2" />
-                <button className="bg-blue-500 p-2 text-white rounded-sm">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                    </svg>
-                </button>
-            </div>
-            </div>
+import { useEffect, useState } from "react";
+
+export default function Chat() {
+  const [ws, setWs] = useState(null);
+  const [onlinePeople, setOnlinePeople] = useState({});
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:4040");
+    setWs(ws);
+    ws.addEventListener("message", handleMessage);
+  }, []);
+  function showOnlinePeople(peopleArray) {
+    const people = {};
+    peopleArray.forEach(({ userId, Username }) => {
+      people[userId] = Username;
+    });
+    setOnlinePeople(people);
+  }
+  function handleMessage(ev) {
+    const messageData = JSON.parse(ev.data);
+    if ("online" in messageData) {
+      showOnlinePeople(messageData.online);
+    }
+  }
+
+  return (
+    <div className="flex h-screen">
+      <div className="bg-white w-1/3 pl-4 pt-4">
+        <div className="text-blue-600 font-bold flex gap-2 mb-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 2c-2.236 0-4.43.18-6.57.524C1.993 2.755 1 4.014 1 5.426v5.148c0 1.413.993 2.67 2.43 2.902.848.137 1.705.248 2.57.331v3.443a.75.75 0 0 0 1.28.53l3.58-3.579a.78.78 0 0 1 .527-.224 41.202 41.202 0 0 0 5.183-.5c1.437-.232 2.43-1.49 2.43-2.903V5.426c0-1.413-.993-2.67-2.43-2.902A41.289 41.289 0 0 0 10 2Zm0 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM8 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm5 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          ChatIPG
         </div>
-    );
+        {Object.keys(onlinePeople).map((userId) => (
+          <div className="border-b border-gray-100 py-2">
+            {onlinePeople[userId]}
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+        <div className="fldex-grow">messages</div>
+        <div className="flex gap-2 ">
+          <input
+            type="text"
+            placeholder="type your message here"
+            className="bg-white flex-grow border rounded-sm p-2"
+          />
+          <button className="bg-blue-500 p-2 text-white rounded-sm">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
